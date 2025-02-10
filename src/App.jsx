0022bcacfa4b0be861c4c90d00f8d01d0a5318b1
Natalie <natali3sen@gmail.com>
2025-02-10 15:40:29 -0800

    Fixed typo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/common/Navbar";
 import Footer from "./components/common/Footer";
 import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/DashBoard";
+import Dashboard from "./pages/Dashboard";
 import LoginForm from "./components/auth/LoginForm";
 import SignupForm from "./components/auth/SignupForm";
 import ProductsPage from "./pages/ProductsPage";
